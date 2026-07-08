7e52e7b58eafd654e39a0ddcca8c689e745d7117
irisabispo <irisabispo@gmail.com>
2026-07-07 20:08:36 -0400

    adiciona catalogo de produtos

---
+++ src/js/components/carousel/catalog.js
@@ -0,0 +1,115 @@
+
+
+const produtos = [
+    {
+        nome: "SUPORTES",
+        categoria: "HARD-TOOLS",
+        descricao: "Suportes para operações de torneamento interno e externo.",
+        imagem: "./src/images/suporte.png",
+        tags: [
+            "TORNEAMENTO INTERNO",
+            "TORNEAMENTO EXTERNO",
+        ]
+    },
+
+    {
+        nome: "BARRA",
+        categoria: "HARD-TOOLS",
+        descricao: "",
+        imagem: "./src/images/suporte.png",
+        tags: [
+            "tag 1",
+            "tag 2",
+        ]
+    },
+
+    {
+        nome: "BROCA",
+        categoria: "HARD-TOOLS",
+        descricao: "",
+        imagem: "./src/images/suporte.png",
+        tags: [
+            "tag 1",
+            "tag 2",
+        ]
+    },
+
+    {
+        nome: "FRESA",
+        categoria: "HARD-TOOLS",
+        descricao: "",
+        imagem: "./src/images/suporte.png",
+        tags: [
+            "tag 1",
+            "tag 2",
+        ]
+    },
+
+    {
+        nome: "INSERTO",
+        categoria: "HARD-TOOLS",
+        descricao: "",
+        imagem: "./src/images/suporte.png",
+        tags: [
+            "tag 1",
+            "tag 2",
+        ]
+    }
+];
+
+let current = 0;
+// essa funçao atualiza a tela
+const title = document.querySelector(".title");
+const categoria = document.querySelector(".categoria");
+const descricao = document.querySelector(".descricao");
+const image = document.querySelector("#imagem-produto");
+const tags = document.querySelector(".tags");
+
+function renderProduct(){
+
+        const produto = produtos[current];
+
+        title.textContent = produto.nome;
+        categoria.textContent = produto.categoria;
+        descricao.textContent = produto.descricao || "";
+        image.src = produto.imagem;
+
+        tags.innerHTML = "";
+
+        produto.tags.forEach(tag => {
+
+            if (tag) {
+
+            const span = document.createElement("span");
+            span.textContent = tag;
+            tags.appendChild(span);
+            }
+        });
+}
+
+// botões
+    const btnNext = document.querySelector("#next");
+    const btnPrev = document.querySelector("#prev");
+
+    btnNext.addEventListener("click", () => {
+        current++;
+
+        if (current >= produtos.length) {
+            current = 0; 
+        }
+
+        renderProduct();
+    });
+
+    btnPrev.addEventListener("click", () => {
+        current--;
+
+        if (current < 0) {
+            current = produtos.length - 1;
+        }
+
+        renderProduct();
+
+    });
+    
+    renderProduct();
